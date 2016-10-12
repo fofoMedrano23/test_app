8ddd83442c8cd2ba6a344c1a5b0c15b9c3a3aa4f
fofo <fofomedrano@gmail.com>
2016-10-12 22:30:14 +0000

    git

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   root 'welcome#home'
   get 'welcome/about' , to: 'welcome#about'
   
+  resources :articles
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
